480db02696d5a994df95924e8b5c4602a85e7820
QuietSwami <fran.abm94@gmail.com>
2016-03-21 15:27:50 +0000

    Fixed asynchronous problem

--- learnyounode/async.js
+++ learnyounode/async.js
@@ -1,16 +1,6 @@
 var fs = require("fs");
-var numbOfLines = 0
 
-function countLines(callback){
-	fs.readFile(process.argv[1], function doneReading(err, fileContents){
-		console.log(fileContents.toString().split('\n'));
-		numbOfLines = fileContents.toString().split('\n').length - 1;
-		callback()
-	});
-}
+fs.readFile(process.argv[2], function doneReading(err, fileContents){
+	console.log(fileContents.toString().split('\n').length - 1);
+});
 
-function printNumber(){
-	console.log(numbOfLines);
-}
-
-countLines(printNumber);
